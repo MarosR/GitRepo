1dc9b73676c03ac83c8687ed45822226078b69e6
Marek Rusnak <maroos.toja@gmail.com>
2014-01-22 09:46:58 +0100

    Make the main project

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "add.h"
+#include "multiply.h"
+#include "pow.h"
+
+void areEquals(double expected, double got)
+{
+	if (expected != got)
+	{
+	  std::cout <<" - Expected "<<expected<<" but got "<<got<<std::endl;
+	}
+	else
+	{
+	  std::cout<<" + Test passed"<<std::endl;
+	}
+}
+
+int main()
+{
+  areEquals(4,add(2,2));
+  areEquals(12, multiply(3,4));
+  areEquals(9, pow(3,2));
+  std::cout<<"End of tests"<<std::endl;
+  return 0;
+}
